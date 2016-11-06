be007eafadf9504f09fad213a72d117b8a6a26e6
kurgm <gogottt2009-hp@yahoo.co.jp>
2016-11-06 21:06:47 +0900

    Update abc047/A/Main.cpp
    https://atcoder.jp/contests/abc047/submissions/970585

---
+++ abc047/A/Main.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  int a[3];
+  for (int i = 0; i < 3; i++) {
+    cin >> a[i];
+  }
+  sort(a, a + 3);
+  if (a[0] + a[1] == a[2]) {
+    cout << "Yes" << endl;
+  } else {
+    cout << "No" << endl;
+  }
+  return 0;
+}
