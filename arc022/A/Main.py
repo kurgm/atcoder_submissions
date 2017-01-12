d318618e91fe0ac80adacf42dba8900d86ddc1a1
kurgm <gogottt2009-hp@yahoo.co.jp>
2017-01-12 11:03:21 +0900

    Update arc022/A/Main.py
    https://atcoder.jp/contests/arc022/submissions/1061623

--- arc022/A/Main.py
+++ arc022/A/Main.py
@@ -1,14 +1,2 @@
-s = raw_input()
-n = len(s)
-
-def l():
-    for i in range(n):
-        if s[i] == 'I' or s[i] == 'i':
-            for j in range(i + 1, n):
-                if s[j] == 'C' or s[j] == 'c':
-                    for k in range(j + 1, n):
-                        if s[k] == 'T' or s[k] == 't':
-                            return True
-    return False
-
-print "YES" if l() else "NO"
+import re
+print "YES" if re.search("i.*c.*t", raw_input(), re.I) else "NO"
