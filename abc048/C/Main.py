ca7b27917f3ed59302ae71f02b15201a73352755
kurgm <gogottt2009-hp@yahoo.co.jp>
2017-01-12 11:17:38 +0900

    Update abc048/C/Main.py
    https://atcoder.jp/contests/arc064/submissions/1061650

--- abc048/C/Main.py
+++ abc048/C/Main.py
@@ -1,17 +1,10 @@
-# -*- coding: utf-8 -*-
-
-n, x = [int(w) for w in raw_input().split()]
-a = [int(w) for w in raw_input().split()]
-
-b = [a[i] + a[i+1] for i in xrange(n - 1)]
-
+n, x = map(int, raw_input().split())
+a = map(int, raw_input().split())
 ans = 0
 for i in xrange(n - 1):
-    if b[i] > x:
-        d = b[i] - x
-        ans += d
-        b[i] = x
-        if i < n - 2:
-            b[i + 1] = b[i + 1] - min(d, a[i + 1])
+    o = a[i] + a[i + 1] - x
+    if o > 0:
+        ans += o
+        a[i + 1] -= min(o, a[i + 1])
 
 print ans
