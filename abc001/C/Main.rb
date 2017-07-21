bbe6d774e2ad5c2d285a9475a8b90e05d69483d1
kurgm <gogottt2009-hp@yahoo.co.jp>
2017-07-21 22:04:25 +0900

    Update abc001/C/Main.rb
    https://atcoder.jp/contests/abc001/submissions/1441917

---
+++ abc001/C/Main.rb
@@ -0,0 +1,1 @@
+a,b=gets.split;puts"%s %d"%[b.to_i<15? ?C:"NNE,NE,ENE,E,ESE,SE,SSE,S,SSW,SW,WSW,W,WNW,NW,NNW,N".split(",")[(a.to_i-113)/225],[*[2,15,33,54,79,107,138,171,207,244,284,326,2e3].each_with_index].bsearch{|x,_|(b.to_i+3)/6<=x}[1]]
