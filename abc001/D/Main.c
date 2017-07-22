b60a1505a750909672d79f6042d2e65dd28bec46
kurgm <gogottt2009-hp@yahoo.co.jp>
2017-07-22 13:54:48 +0900

    Update abc001/D/Main.c
    https://atcoder.jp/contests/abc001/submissions/1442969

--- abc001/D/Main.c
+++ abc001/D/Main.c
@@ -1,1 +1,1 @@
-a[481];u;v;s;e;i;main(){for(scanf("%d",&i);i--;a[s/5]++,a[(e+=4)/5+e%100/60*8]--)scanf("%d-%d",&s,&e);for(;++i<481;!(v=u)^!(u+=a[i])&&printf("%04d%c",i*5,u?45:10));}
+a[481];u;v;s;e;i;main(){for(scanf("%d",&i);i--;a[s/5]++,a[(e+4)/5+e%100/56*8]--)scanf("%d-%d",&s,&e);for(;++i<481;!(v=u)^!(u+=a[i])&&printf("%04d%c",i*5,u?45:10));}
