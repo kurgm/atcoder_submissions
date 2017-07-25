bb004804fd5b56f5450292b195636f9444d5c4dd
kurgm <gogottt2009-hp@yahoo.co.jp>
2017-07-25 22:40:58 +0900

    Update abc002/D/Main.py
    https://atcoder.jp/contests/abc002/submissions/1453561

--- abc002/D/Main.py
+++ abc002/D/Main.py
@@ -1,7 +1,4 @@
-import itertools
-n,m=map(int,input().split());d={i:{i}for i in range(n)};a=1
-for _ in range(m):x,y=[int(t)-1 for t in input().split()];d[x].add(y);d[y].add(x)
-for j in itertools.product([0,1],repeat=n):
- o=[d[i]for i,k in enumerate(j)if k];l=len(o)
- if l==len(set(range(n)).intersection(*(o or[[]]))):a=max(a,l)
-print(a)
+from itertools import combinations as c
+f=lambda:map(int,input().split());n,m=f();r=range;d={tuple(f())for _ in r(m)}    
+for a in r(1,n+2):    
+ if not any(d>=set(c(o,2))for o in c(r(1,n+1),a)):print(a-1);break
