346fdbbfd47795484d970f01088e40d72441ccfb
kurgm <gogottt2009-hp@yahoo.co.jp>
2017-07-25 22:59:38 +0900

    Update abc002/D/Main.py
    https://atcoder.jp/contests/abc002/submissions/1453593

--- abc002/D/Main.py
+++ abc002/D/Main.py
@@ -1,4 +1,3 @@
 import itertools as t;c=t.combinations
 f=lambda:map(int,input().split());n,m=f();d={tuple(f())for _ in range(m)};r=range(1,n+1)
-for a in r:
- if not any(d>=set(c(o,2))for o in c(r,a+1)):print(a);break
+all(1 if any(d>=set(c(o,2))for o in c(r,a+1))else print(a)for a in r)
