fa2d8dbc6a2225a0f68f92c47cc8fb9ba267b84a
kurgm <gogottt2009-hp@yahoo.co.jp>
2017-07-25 23:04:19 +0900

    Update abc002/D/Main.py
    https://atcoder.jp/contests/abc002/submissions/1453603

--- abc002/D/Main.py
+++ abc002/D/Main.py
@@ -1,3 +1,1 @@
-import itertools as t;c=t.combinations
-f=lambda:map(int,input().split());n,m=f();d={tuple(f())for _ in range(m)};r=range(1,n+1)
-all(1 if any(d>=set(c(o,2))for o in c(r,a+1))else print(a)for a in r)
+import itertools as t;c=t.combinations;f=lambda:map(int,input().split());n,m=f();d={tuple(f())for _ in range(m)};r=range(1,n+1);all(any(d>=set(c(o,2))for o in c(r,a+1))or print(a)for a in r)
