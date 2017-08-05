ffaa099ff28ada6e472f1f5b39e8c2a752befb5f
kurgm <gogottt2009-hp@yahoo.co.jp>
2017-08-05 23:07:17 +0900

    Update abc009/C/Main.rb
    https://atcoder.jp/contests/abc009/submissions/1482584

--- abc009/C/Main.rb
+++ abc009/C/Main.rb
@@ -1,1 +1,1 @@
-n,k,s=`ul`.split;m=k.to_i;s=s.chars;puts [*c=s.sort].map{k=m;v=s.shift;c.delete_at c.index{|d|f=-m=v[d]?k:k-1;(?a..?z).map{|e|f+=(s.count(e)-c.count(e)+(e[d]?1:0)).abs};f<=m}}*""
+n,k,s=`ul`.split;m=k.to_i;s=s.chars;puts [*c=s.sort].map{k=m;v=s.shift;c.delete_at c.index{|d|f=m=v[d]?k:k-1;(?a..?z).map{|e|f-=(s.count(e)-c.count(e)+1[e<=>d]).abs};f>~m}}*""
