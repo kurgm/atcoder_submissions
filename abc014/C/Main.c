13550ed9b6d3d3681490fbc25414ebdcb5b9d39e
kurgm <gogottt2009-hp@yahoo.co.jp>
2018-08-24 23:28:01 +0900

    Update abc014/C/Main.c
    https://atcoder.jp/contests/abc014/submissions/3067142

---
+++ abc014/C/Main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct elem {
+    int index;
+    int diff;
+} elem_t;
+
+static int compar(const void *a, const void *b) {
+    const elem_t *a_ = a, *b_ = b;
+    return a_->index - b_->index;
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    elem_t *ary = malloc(sizeof(elem_t) * (2 * n));
+    for (int i = 0; i < 2 * n; i += 2) {
+        int a, b;
+        scanf("%d%d", &a, &b);
+        ary[i].index = a;
+        ary[i].diff = 1;
+        ary[i + 1].index = b + 1;
+        ary[i + 1].diff = -1;
+    }
+
+    qsort(ary, 2 * n, sizeof(elem_t), compar);
+
+    int ans = 0, cur = 0;
+    for (int i = 0; i < 2 * n; i++) {
+        for (; i + 1 < 2 * n && ary[i].index == ary[i + 1].index; i++) {
+            cur += ary[i].diff;
+        }
+        cur += ary[i].diff;
+        ans = ans > cur ? ans : cur;
+    }
+    free(ary);
+    printf("%d\n", ans);
+    return 0;
+}
