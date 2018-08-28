a42746eda8f1cc681e6cbe7ed96c7daac6560f64
kurgm <gogottt2009-hp@yahoo.co.jp>
2018-08-28 14:10:38 +0900

    Update abc015/C/Main.py
    https://atcoder.jp/contests/abc015/submissions/3092169

--- abc015/C/Main.py
+++ abc015/C/Main.py
@@ -1,4 +1,1 @@
-import itertools as I
-f=open(0)
-next(f)
-print(["Found","Nothing"][all(eval("^".join(p))for p in I.product(*[l.split()for l in f]))])
+import itertools as I;f=open(0);next(f);print(["Found","Nothing"][all(eval("^".join(p))for p in I.product(*[l.split()for l in f]))])
