a388fd72dea9cf91e63dca4712eb6078afaf0570
kurgm <gogottt2009-hp@yahoo.co.jp>
2020-03-14 21:17:44 +0900

    Update abc066/A/Main.py
    https://atcoder.jp/contests/abc066/submissions/10846447

---
+++ abc066/A/Main.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+import sys
+
+
+def solve(a: int, b: int, c: int):
+    print(a + b + c - max(a, b, c))
+
+
+def main():
+    def iterate_tokens():
+        for line in sys.stdin:
+            for word in line.split():
+                yield word
+    tokens = iterate_tokens()
+    a = int(next(tokens))  # type: int
+    b = int(next(tokens))  # type: int
+    c = int(next(tokens))  # type: int
+    solve(a, b, c)
+
+
+if __name__ == '__main__':
+    main()
